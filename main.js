f8bf875e1c0d9811b400d58c81da9cab1d4973c5
jeemoh-reeds <85808311+jeemoh-reeds@users.noreply.github.com>
2021-06-13 15:14:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+<<<<<<< HEAD
+//To call the endpoint for AWIN and AMAZON
+async function saveNetworkConfig(name, config) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    name: name,
+    config: config,
+  });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch("/api/admin/network/", requestOptions);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log("error", error);
+    throw error;
+  }
+}
+
+//To keep the inputed parameters for AWIN and AMAZON on refresh 
+async function getNetworkConfig(name) {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+  try {
+    const response = await fetch("/api/admin/network/" + name, requestOptions);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log("error", error);
+    return null;
+  }
+}
+=======
+//To call the endpoint for AWIN and AMAZON
+async function saveNetworkConfig(name, config) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    name: name,
+    config: config,
+  });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch("/api/admin/network/", requestOptions);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log("error", error);
+    throw error;
+  }
+}
+
+//To keep the inputed parameters for AWIN and AMAZON on refresh 
+async function getNetworkConfig(name) {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+  try {
+    const response = await fetch("/api/admin/network/" + name, requestOptions);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log("error", error);
+    return null;
+  }
+}
+>>>>>>> f97ad0ca4f4c296e341c9fd9ef15cc7bf3e15488
